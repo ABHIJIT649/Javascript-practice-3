b8d971d97f22df2cdb499db2b7041cadf4a5ac31
amiyadas01 <amiyadas708@gmail.com>
2025-06-05 19:00:42 +0530

    add array

--- besic 02/array.js
+++ besic 02/array.js
@@ -1,38 +1,38 @@
-// //Arrays
+//Arrays
 
-// // array is a object and storing of multiple items under a variable
-// const myArr = [0, 1, 2, 4, 7, "abhijit"];
+// array is a object and storing of multiple items under a variable
+const myArr = [0, 1, 2, 4, 7, "abhijit"];
 
-// console.log(myArr[5])
+console.log(myArr[5])
 
-// //array-copy-operation create sh allow copies
+//array-copy-operation create sh allow copies
 
-// const myHeros = ["superman" ,"ironman" , "thor"];
+const myHeros = ["superman" ,"ironman" , "thor"];
 
-// const myArr2 = new Array(1,2,3,4);
+const myArr2 = new Array(1,2,3,4);
 
-// console.log(myArr2[1])
-
-
-// //Array method
+console.log(myArr2[1])
 
 
-// myArr.push(6) //add value in array last
-// myArr.push(9)
-// myArr.pop() //remove lvalue in  array last 
-// console.log(myArr) 
-
-// myArr.unshift(9) // add value in  array start
-// myArr.shift() // remove value in  array start
-// console.log(myArr)
+//Array method
 
 
-// console.log(myArr.includes(9)); //question in arry 9 present he ya nehi
-// console.log(myArr.indexOf(4)); //value kis index pe he show karega
+myArr.push(6) //add value in array last
+myArr.push(9)
+myArr.pop() //remove lvalue in  array last 
+console.log(myArr) 
 
-// const newArr = myArr.join()
-// console.log(myArr)
-// console.log( newArr) //change string
+myArr.unshift(9) // add value in  array start
+myArr.shift() // remove value in  array start
+console.log(myArr)
+
+
+console.log(myArr.includes(9)); //question in arry 9 present he ya nehi
+console.log(myArr.indexOf(4)); //value kis index pe he show karega
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log( newArr) //change string
 
 
 // //Slice , Splice
