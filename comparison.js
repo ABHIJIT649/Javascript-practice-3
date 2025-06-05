1ae6d2897b4d232cff89814adb474081bc94e59f
amiyadas01 <amiyadas708@gmail.com>
2025-06-05 13:58:45 +0530

    add comarison

---
+++ comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 >1);
+// console.log(2>= 1);
+// console.log(2 <= 1);
+// console.log(3 == 3);
+// console.log(1!= 2);
+
+
+
+// console.log("2" >1)
+// console.log("02" >1)
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+
+
+// === (not only chack number check data type string / number /boolean)
+
+
+console.log("2" === 2); //false becuse string or number is not same
