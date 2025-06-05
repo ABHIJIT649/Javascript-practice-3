f7b31a1a385a85bd3db79d58ee96bb33afb33f93
amiyadas01 <amiyadas708@gmail.com>
2025-06-05 13:05:32 +0530

    conversion of fdata type

---
+++ conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 33  // ans number
+// let score = "33" //ans string
+
+
+// console.log(typeof score)
+
+console.log(typeof score)
+
+
+
+let value = "67abc" //(value =null =>ans 0, value 
+//= undefined =>ans1,value = ture =>1 false =>0,)
+
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+
+let isLoggedIn = "abhijit"
+
+
+let booleanLoggedIn = Boolean(isLoggedIn) //string to  boolean value
+console.log(booleanLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"abhijit" => true
+
+
+let someNumber = 33
+
+
+let stringNumber = String(someNumber) //converet number to string
+console.log( stringNumber); 
+console.log(typeof stringNumber)
