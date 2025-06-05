ec300112e62fcdd0389bd207c1e23fb337005733
amiyadas01 <amiyadas708@gmail.com>
2025-06-05 13:48:26 +0530

    add operation

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,37 +1,98 @@
-let score = 33  // ans number
-// let score = "33" //ans string
+// let score = 33  // ans number
+// // let score = "33" //ans string
 
+
+// // console.log(typeof score)
 
 // console.log(typeof score)
 
-console.log(typeof score)
+
+
+// let value = "67abc" //(value =null =>ans 0, value 
+// //= undefined =>ans1,value = ture =>1 false =>0,)
+
+// let valueInNumber = Number(value)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 
-let value = "67abc" //(value =null =>ans 0, value 
-//= undefined =>ans1,value = ture =>1 false =>0,)
-
-let valueInNumber = Number(value)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let isLoggedIn = "abhijit"
 
 
+// let booleanLoggedIn = Boolean(isLoggedIn) //string to  boolean value
+// console.log(booleanLoggedIn);
 
-let isLoggedIn = "abhijit"
-
-
-let booleanLoggedIn = Boolean(isLoggedIn) //string to  boolean value
-console.log(booleanLoggedIn);
-
-//1 => true
-//0 => false
-//"" => false
-//"abhijit" => true
+// //1 => true
+// //0 => false
+// //"" => false
+// //"abhijit" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
 
-let stringNumber = String(someNumber) //converet number to string
-console.log( stringNumber); 
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber) //converet number to string
+// console.log( stringNumber); 
+// console.log(typeof stringNumber)
+
+
+// // ****************************** Operation *****************************//
+
+
+let value =3
+let negtiveValue = -value
+// console.log(negtiveValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+let str1 = "hello"
+let str2 = " abhijit"
+
+let str3 = str1+str2;
+// console.log(str3)
+
+
+// console.log(1 + "2") //1 2
+// console.log("1" +2) //12
+// console.log("1" + 2 +2); //122 string first
+
+// console.log( 1 + 2 + "2"); // ans 32  string last
+
+
+// console.log(+true)//ans =>1 becuse true is boolean
+// console.log(true) // ans =>true
+// console.log(+"") //ans =>0
+
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter =100
+gameCounter++; 
+console.log(gameCounter)
+
+
+//postfix
+let x= 3;
+let y = x++;
+console.log(`x:${x} , y:${y}`)
+
+//prefix
+let a = 3;
+let b = ++a;
+console.log(`a:${a}, b:${b}`)
+ 
+
+
