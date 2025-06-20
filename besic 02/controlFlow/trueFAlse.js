eb3bc556363a94b381aee683d2e589e6bb3a145d
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-06-20 22:51:32 +0530

    add true false in practice

---
+++ besic 02/controlFlow/trueFAlse.js
@@ -0,0 +1,13 @@
+const userEmail = "h@abhijit.ai"
+
+if (userEmail){ //assume karke true 
+    console.log("Got user email");
+
+}else{
+    console.log("dont have user email");
+}
+
+// false value 
+
+// false value => false, 0=>Zero, -0, BigInt 0n, "", null, undefind, NoN
+
