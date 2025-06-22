19b5e5a24734826dfc1675809467c7679a1e43bf
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-06-22 19:10:34 +0530

    while or do while loop

---
+++ whileLoop.js
@@ -0,0 +1,28 @@
+// while loop
+let i = 0;
+while (i <= 10 ) {
+    console.log(`value of i is ${i}`);
+    i = i+2;
+}
+
+// array in whilw loop
+
+let myArray = ["flash", "superman", "ironman","batman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// ********** do while loop ************ //
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 20);
