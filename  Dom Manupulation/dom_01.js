9e8d640c6d44ff1db7007828d2ac0665c056c9af
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-06-25 16:35:00 +0530

    add dom practice

---
+++  Dom Manupulation/dom_01.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName('list-item')
+const myTEmplist = document.getElementsByClassName('list-item')
+
+const myConAry = Array.from(myTEmplist);
+
+myTEmplist.forEach(function (li) {
+    li.
+})
