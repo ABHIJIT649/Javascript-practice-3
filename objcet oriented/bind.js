0103e5ad2ac1839f0d415bd6d5294adfa8cb0e83
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-28 19:31:59 +0530

    add bind

--- objcet oriented/bind.js
+++ objcet oriented/bind.js
@@ -1,21 +1,50 @@
-class React {
-    constructor() {
-        this.library = "react"
-        this.server = "https://localhost:300"
+// class React {
+//     constructor() {
+//         this.library = "react"
+//         this.server = "https://localhost:300"
 
 
-        //requirement
+//         //requirement
 
-        document.querySelector('button')
-        .addEventListener('click', this.handleClick
-            .bind(this)
-        )
-    }
+//         document.querySelector('button')
+//         .addEventListener('click', this.handleClick
+//             .bind(this)
+//         )
+//     }
 
-    handleClick(){
-        console.log("Button clicked");
-        console.log(this.server);
-    }
+//     handleClick(){
+//         console.log("Button clicked");
+//         console.log(this.server);
+//     }
+// }
+
+// const app = new React()
+
+
+
+//Example
+
+const persion = { name: "Abhi"}
+
+function greet(){
+    console.log("hello, " + this.name);
 }
 
-const app = new React()
+const bindGreet = greet.bind(persion)
+bindGreet()
+
+// . Bind greet() to another object
+
+const student1 = {
+    name: "rohit",
+    greet : function () {
+    console.log("hello ,"  + this.name);
+   }
+};
+
+const student2 = {name: "Vira"}
+
+const bindAnotherObj = student1.greet.bind(student2);
+bindAnotherObj()
+
+
