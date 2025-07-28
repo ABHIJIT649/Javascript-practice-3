f8c484215acd611c15ab7917314d3a670935b810
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-28 21:48:58 +0530

    mathpi aDD

--- objcet oriented/mathpai.js
+++ objcet oriented/mathpai.js
@@ -1,1 +1,40 @@
-Object.
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI" )
+
+console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+// const myobj = Object.create( null)
+
+const chai = {
+    name : "ginger chai",
+    price : 250,
+    isAvailable : true,
+
+
+    orderChai: function(){
+        console.log("chai is not rady");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai, 'name',{
+    // writable: false,
+    enumerable : true,
+
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+    console.log(`${key} : ${value}`);
+
+        
+    }
+}
+
+
